7a622c55c0d79230e9b9bfbea929d9d67b330c07
Ronith <87087292+ronith256@users.noreply.github.com>
2023-07-25 02:08:50 +0530

    Dummy Project

---
+++ app/src/main/java/com/lucario/example/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lucario.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(e -> Toast.makeText(MainActivity.this, "Button pressed", Toast.LENGTH_SHORT).show());
+    }
+}
